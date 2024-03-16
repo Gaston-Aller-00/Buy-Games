9478dda424d60e6ab5c2d300a374928ff1b238d1
Gaston <gastonallerdev@gmail.com>
2024-03-15 21:06:35 -0300

    Featured productss

---
+++ components/FeaturedProduct.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import FeaturedProductsSlider from "./Helper/FeaturedProductsSlider";
+
+const FeaturedProduct = () => {
+  return (
+    <div className="pt-[6rem] pb-[3rem] bg-gray-900">
+      <div className="w-[80%] mx-auto flex items-center justify-between">
+        <h1 className="md:text-[28px] text-[22px] lg:text-[34px] text-white">
+          Featured Producst
+        </h1>
+        <button className="uppercase text-[13px] md:text-[15px] text-yellow-500 ">View All Products</button>
+      </div>
+      <div className="w-[80%] mt-[2rem] mx-auto">
+            {/* FeaturedProductsSlider */}
+            <FeaturedProductsSlider/>
+
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProduct;
