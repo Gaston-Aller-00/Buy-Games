2acdcddd9c78eae7d51da86391eee450ab8362c7
Gaston <gastonallerdev@gmail.com>
2024-03-19 01:07:26 -0300

    new & topS

--- components/News.tsx
+++ components/News.tsx
@@ -4,15 +4,15 @@ import NewsCards from "./Helper/NewsCards";
 const News = () => {
   return (
     <div className="pt-[5rem]  pb-[3rem] bg-gray-800">
-      <h1 className="w-[80%] mx-auto md:text-[28px] text-[22px] lg:text-[34px] text-white">
+      <h2 className="w-[80%] mx-auto md:text-[28px] text-[22px] lg:text-[34px] text-white">
         Latest News
-      </h1>
+      </h2>
       <div className="mt-[2rem]">
         <div className="grid grid-cols-1 md:grid-cols-6 gap-[3rem] w-[80%] mx-auto ">
           {/* cards Newss */}
-          <NewsCards image="/images/g2.jpg" title="New Update News"/>
-          <NewsCards image="/images/g4.jpg" title="Check whats new"/>
-          <NewsCards image="/images/g7.jpg" title="New Tournament"/>
+          <NewsCards image="/images/g2.jpg" title="New Update News"  description={`"Players of the highly anticipated RPG title, "Epic Quest: Rise of the Dragons," are in for an immersive treat as developers announce the integration of cutting-edge virtual reality technology. Dive headfirst into the fantastical world of dragons and magic like never before, as you wield your sword and shield in breathtaking VR environments. Get ready to embark on an epic adventure like no other, as "Epic Quest" sets the standard for VR gaming excellence.`}/>
+          <NewsCards image="/images/g4.jpg" title="Check whats new" description={`"Galactic Conquest: New Frontiers" expansion pack promises interstellar thrills: Brace yourselves, space adventurers, as "Galactic Conquest: New Frontiers" unveils its latest expansion pack, promising an array of thrilling new challenges and adventures. Set your sights on uncharted territories, engage in epic space battles, and forge alliances with alien species as you expand your galactic empire. With enhanced graphics, new missions, and dynamic gameplay features, this expansion pack is sure to take your space-faring experience to infinity and beyond.`}/>
+          <NewsCards image="/images/g7.jpg" title="New Tournament" description={`"Mystic Mayhem: The Lost Relic" launches global treasure hunt event: Calling all intrepid explorers and puzzle enthusiasts! "Mystic Mayhem: The Lost Relic" has launched an unprecedented global treasure hunt event, inviting players from around the world to embark on a quest for fame and fortune. Unravel ancient mysteries, decipher cryptic clues, and uncover hidden relics as you traverse exotic locations and brave perilous challenges. With exclusive in-game rewards and the chance to leave your mark on gaming history, this is one event you won't want to miss. Join the hunt today and become a legend in "Mystic Mayhem: The Lost Relic!"`}/>
         </div>
       </div>
     </div>
--- components/TopProducts.tsx
+++ components/TopProducts.tsx
@@ -5,9 +5,9 @@ const TopProducts = () => {
   return (
     <div className="bg-gray-800 pt-[4rem] pb-[3rem] px-[12rem] ">
       <div className="w-[80%] mx-auto flex items-center justify-between">
-        <h1 className="md:text-[28px] text-[22px] lg:text-[34px] text-white">
+        <h2 className="md:text-[28px] text-[22px] lg:text-[34px] text-white">
           Top Products
-        </h1>
+        </h2>
         <button className="uppercase text-[13px] md:text-[15px] text-yellow-400">
           {" "}
           View All Products
@@ -17,55 +17,75 @@ const TopProducts = () => {
         className=" grid  mt-[2rem]  grid-cols-1 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4 gap-y-8 w-
       [%80] mx-auto "
       >
-        <div>
-          <ProductCard
-            title="COD"
-            actualPrice="$60"
-            discountPrice="$50"
-            category="Shotting"
-            image="/images/g1.jpg"
-          />
+        <div data-aos="fade-right" data-aos-anchor-placement="top-center">
+        <ProductCard
+        title="COD"
+        actualPrice="$60"
+        discountPrice="$50"
+        category="Shotting"
+        image="/images/g1.jpg"
+      />
         </div>
 
-        <div>
+        <div
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="150"
+        >
           <ProductCard
-            title="COD"
-            actualPrice="$60"
-            discountPrice="$50"
-            category="Shotting"
-            image="/images/g2.jpg"
-          />
+        title="Fury Road"
+        actualPrice="$40"
+        discountPrice="$35"
+        category="Shotting"
+        image="/images/g2.jpg"
+      />
         </div>
-        <div>
+        <div
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="150"
+        >
           <ProductCard
-            title="COD"
-            actualPrice="$60"
-            discountPrice="$50"
-            category="Shotting"
-            image="/images/g3.jpg"
-          />
+        title="Ghost Town"
+        actualPrice="$60"
+        discountPrice="$50"
+        category="Shotting"
+        image="/images/g5.jpg"
+      />
         </div>
-        <div>
+        <div
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="300"
+        >
           <ProductCard
-            title="COD"
-            actualPrice="$60"
-            discountPrice="$50"
-            category="Shotting"
-            image="/images/g4.jpg"
-          />
+        title="Eclipse of Destiny"
+        actualPrice="$60"
+        discountPrice="$50"
+        category="Shotting"
+        image="/images/g4.jpg"
+      />
         </div>
-        <div>
-          <ProductCard
-            title="COD"
-            actualPrice="$60"
-            discountPrice="$50"
-            category="Shotting"
-            image="/images/g5.jpg"
-          />
+        <div
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="300"
+        >
+         <ProductCard
+        title="Ghost Town"
+        actualPrice="$60"
+        discountPrice="$50"
+        category="Shotting"
+        image="/images/g5.jpg"
+      />
         </div>
-        <div>
+        <div
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="300"
+        >
           <ProductCard
-            title="COD"
+            title="Battlefield"
             actualPrice="$60"
             discountPrice="$50"
             category="Shotting"
@@ -73,29 +93,36 @@ const TopProducts = () => {
           />
         </div>
 
-        <div>
-          <ProductCard
-            title="COD"
-            actualPrice="$60"
-            discountPrice="$50"
-            category="Shotting"
-            image="/images/g7.jpg"
-          />
+        <div
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="400"
+        >
+         <ProductCard
+        title="Days gone"
+        actualPrice="$30"
+        discountPrice="$25"
+        category="Shotting"
+        image="/images/g8.jpg"
+      />
         </div>
-        <div>
-          <ProductCard
-            title="COD"
-            actualPrice="$60"
-            discountPrice="$50"
-            category="Shotting"
-            image="/images/g8.jpg"
-          />
+        <div
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="400"
+        >
+       <ProductCard
+        title="Dark Horizon"
+        actualPrice="$50"
+        discountPrice="$30"
+        category="Shotting"
+        image="/images/g7.jpg"
+      />
         </div>
-   
-      
       </div>
     </div>
   );
 };
 
 export default TopProducts;
+
