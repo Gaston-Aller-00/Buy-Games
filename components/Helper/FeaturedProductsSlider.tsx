a2357b46e2acb025faaa3d140d6566080074675c
Gaston <gastonallerdev@gmail.com>
2024-03-20 03:44:59 -0300

    Slider

--- components/Helper/FeaturedProductsSlider.tsx
+++ components/Helper/FeaturedProductsSlider.tsx
@@ -7,17 +7,17 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1300 },
     items: 4,
-    slidesToSlide: 1, // optional, default to 1.
+    slidesToSlide: 1, 
   },
   tablet: {
     breakpoint: { max: 1300, min: 764 },
     items: 3,
-    slidesToSlide: 1, // optional, default to 1.
+    slidesToSlide: 1, 
   },
   mobile: {
     breakpoint: { max: 764, min: 0 },
     items: 2,
-    slidesToSlide: 1, // optional, default to 1.
+    slidesToSlide: 1, 
   },
 };
 const FeaturedProductsSlider = () => {
@@ -26,7 +26,7 @@ const FeaturedProductsSlider = () => {
       additionalTransfrom={0}
       arrows={true}
       autoPlay={true}
-      autoPlaySpeed={5000}
+      autoPlaySpeed={3000}
       centerMode={false}
       infinite
       responsive={responsive}
@@ -42,51 +42,51 @@ const FeaturedProductsSlider = () => {
       />
 
       <ProductCard
-        title="COD"
-        actualPrice="$60"
-        discountPrice="$50"
+        title="Fury Road"
+        actualPrice="$40"
+        discountPrice="$35"
         category="Shotting"
         image="/images/g2.jpg"
       />
       <ProductCard
-        title="COD"
-        actualPrice="$60"
-        discountPrice="$50"
+        title="Shadow's Edge"
+        actualPrice="$55"
+        discountPrice="$25"
         category="Shotting"
         image="/images/g3.jpg"
       />
       <ProductCard
-        title="COD"
+        title="Eclipse of Destiny"
         actualPrice="$60"
         discountPrice="$50"
         category="Shotting"
         image="/images/g4.jpg"
       />
       <ProductCard
-        title="COD"
+        title="Ghost Town"
         actualPrice="$60"
         discountPrice="$50"
         category="Shotting"
         image="/images/g5.jpg"
       />
       <ProductCard
-        title="COD"
-        actualPrice="$60"
-        discountPrice="$50"
+        title="Doomed Soul"
+        actualPrice="$40"
+        discountPrice="$20"
         category="Shotting"
         image="/images/g6.jpg"
       />
       <ProductCard
-        title="COD"
-        actualPrice="$60"
-        discountPrice="$50"
+        title="Dark Horizon"
+        actualPrice="$50"
+        discountPrice="$30"
         category="Shotting"
         image="/images/g7.jpg"
       />
       <ProductCard
-        title="COD"
-        actualPrice="$60"
-        discountPrice="$50"
+        title="Days gone"
+        actualPrice="$30"
+        discountPrice="$25"
         category="Shotting"
         image="/images/g8.jpg"
       />
